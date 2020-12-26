ce5a510ffba71009046529cc9107e906ef2b394e
sitetesting <69781282+sitetesting@users.noreply.github.com>
2020-12-26 13:50:45 -0500

    new feature

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-function price(){
-    const  elementSelected  = document.querySelector(".cake")
+function price(price, item){
+    const  elementSelected  = document.querySelector(item)
     const brtag = document.createElement("br","")
     elementSelected.appendChild(brtag)
     const span  =  document.createElement("span")
     span.classList.add("rate")
 
-    const whatPrice = document.createTextNode("$ 20")
+    const whatPrice = document.createTextNode(price)
     
     span.appendChild(whatPrice)
 
     elementSelected.appendChild(span)
 }
 
-price()
+price("$ 20",".cake")
+price("$ 10",".bread")
+price("$ 5",".pastry")
+price("$ 8",".icecream")
+price("$ 10",".pies")
+price("$ 5",".premix")
+price("$ 4",".cone")
+price("$ 6",".sweets")
+price("$ 7",".muffin")
+price("$ 9",".toast")
